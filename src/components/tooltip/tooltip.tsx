ee5429b0093bd2680e3c17049468bb8a17ec3f9b
Alexander Widua <a.widua@gmail.com>
2022-02-15 19:34:18 +0100

    add: tooltip component

---
+++ src/components/tooltip/tooltip.tsx
@@ -0,0 +1,41 @@
+import { h, ComponentChildren } from 'preact'
+import { createClassName } from '@create-figma-plugin/ui'
+import styles from './tooltip.css'
+
+export type Alignment =
+	| 'left'
+	| 'topLeft'
+	| 'top'
+	| 'topRight'
+	| 'right'
+	| 'bottomRight'
+	| 'bottom'
+	| 'bottomLeft'
+
+export type TooltipValue = {
+	text: string
+	children: ComponentChildren
+}
+
+export interface TooltipProps {
+	value: TooltipValue
+	alignment?: Alignment
+	delay: number
+}
+
+export function Tooltip({
+	value,
+	alignment = 'top',
+	delay = 0.2
+}: TooltipProps) {
+	return (
+		<span className={styles.tooltip}>
+			{value.text}
+			<span
+				className={createClassName([styles.text, styles[alignment]])}
+				style={{ transitionDelay: `${delay}s` }}>
+				{value.children}
+			</span>
+		</span>
+	)
+}
